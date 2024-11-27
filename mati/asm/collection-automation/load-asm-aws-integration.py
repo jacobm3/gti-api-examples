0012ffefc8d0c3f6050593937487a82e6d0bae88
Jacob <jacobm3@gmail.com>
2024-11-27 07:20:37 +0000

    Update load-asm-aws-integration.py

--- mati/asm/collection-automation/load-asm-aws-integration.py
+++ mati/asm/collection-automation/load-asm-aws-integration.py
@@ -5,6 +5,9 @@
 #
 # jmarts@google.com
 #
+# Wed Nov 27 06:59:47 UTC 2024
+# 
+
 
 import argparse
 import csv
@@ -18,32 +21,37 @@ import time
 
 from pprint import pprint
 
+
 # Configure logging
 def custom_time(*args):
     dt = datetime.datetime.now()
-    return dt.isoformat(sep=' ', timespec='milliseconds')
+    return dt.timetuple()
 
-logging.basicConfig(
-    filename='asm_integration.log',
-    level=logging.DEBUG,
-    format='%(custom_time)s - %(levelname)s - %(message)s'
-)
+# Custom formatter with asctime utilizing custom_time
+class CustomFormatter(logging.Formatter):
+    def formatTime(self, record, datefmt=None):
+        # Use the custom time formatter
+        ct = custom_time()
+        return time.strftime('%Y-%m-%d %H:%M:%S.%f', ct)[:-3]
 
-logging.Formatter.converter = custom_time
+formatter = CustomFormatter('%(asctime)s - %(levelname)s - %(message)s')
 
-#logging.basicConfig(
-#    filename='asm_integration.log',
-#    level=logging.DEBUG,
-#    format='%(asctime)s.%(msecs)03d%(timezone)s - %(levelname)s - %(message)s',
-#    datefmt='%Y-%m-%dT%H:%M:%S',
-#)
+# Configure logging to file
+file_handler = logging.FileHandler('asm_integration.log')
+file_handler.setLevel(logging.DEBUG)
+file_handler.setFormatter(formatter)
 
-# Create a handler to output to stdout as well
+# Configure logging to console
 console_handler = logging.StreamHandler()
-console_handler.setLevel(logging.DEBUG)  # Set the level for console output
-formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
+console_handler.setLevel(logging.DEBUG)
 console_handler.setFormatter(formatter)
-logging.getLogger().addHandler(console_handler) 
+
+# Root logger configuration
+logging.basicConfig(level=logging.DEBUG, handlers=[file_handler, console_handler])
+
+logging.info("Logging system initialized.")
+
+
 
 def get_headers(makey, mskey, project_id):
     """Returns the headers for ASM API requests."""
@@ -55,9 +63,42 @@ def get_headers(makey, mskey, project_id):
         "PROJECT_ID": str(project_id),
     }
 
+def create_user_collection(headers, name, workflow_name):
+    """Creates a user collection in ASM."""
+
+    # Reference
+    # https://docs.mandiant.com/home/asm-api#tag/Collections/paths/~1api~1v1~1user_collections/post
+    # https://gtidocs.virustotal.com/reference/post_user-collections
+
+    time.sleep(1)
+
+    url = "https://asm-api.advantage.mandiant.com/api/v1/user_collections"
+    data = {"name": name, "workflow_name": workflow_name}
+
+    logging.debug(f"POST {url}")
+    logging.debug(f"Data: {data}")
+
+    if args.dry_run:
+        return
+    response = requests.post(url, headers=headers, json=data)
+
+    if response.status_code == 200:
+        logging.info("Collection created successfully!")
+        logging.debug(f"Response: {response.json()}")
+        return(response.json())
+    else:
+        logging.error(f"Error creating collection: {response.status_code}")
+        logging.error(f"Response: {response.text}")
+        sys.exit(1)
 
 def create_integration(headers, project_uuid, name, role_arn):
     """Creates an integration in ASM."""
+
+    # Reference
+    # https://docs.mandiant.com/home/asm-api#tag/Integrations/paths/~1api~1v1~1projects~1%7Buuid%7D~1integrations/post
+    # https://gtidocs.virustotal.com/reference/post_projects-uuid-integrations
+
+    time.sleep(1)
 
     url = f"https://asm-api.advantage.mandiant.com/api/v1/projects/{project_uuid}/integrations"
     data = {"name": name, "secrets": {"aws_role_arn": role_arn}, "type": "AwsCredential"}
@@ -79,31 +120,15 @@ def create_integration(headers, project_uuid, name, role_arn):
     return response.text
 
 
-def create_user_collection(headers, name, workflow_name):
-    """Creates a user collection in ASM."""
-
-    url = "https://asm-api.advantage.mandiant.com/api/v1/user_collections"
-    data = {"name": name, "workflow_name": workflow_name}
-
-    logging.debug(f"POST {url}")
-    logging.debug(f"Data: {data}")
-
-    if args.dry_run:
-        return
-    response = requests.post(url, headers=headers, json=data)
-
-    if response.status_code == 200:
-        logging.info("Collection created successfully!")
-        logging.debug(f"Response: {response.json()}")
-        return(response.json())
-    else:
-        logging.error(f"Error creating collection: {response.status_code}")
-        logging.error(f"Response: {response.text}")
-        sys.exit(1)
-
 
 def link_collection_integration(headers, collection_uuid, integration_uuid):
     """Links an integration to a collection in ASM."""
+
+    # Reference
+    # https://docs.mandiant.com/home/asm-api#tag/Integration-Collections
+    # https://gtidocs.virustotal.com/reference/post_user-collections-collection-uuid-integration-collections
+
+    time.sleep(1)
 
     url = f"https://asm-api.advantage.mandiant.com/api/v1/user_collections/{collection_uuid}/integration_collections"
     data = {"integration_uuid": integration_uuid}
@@ -122,9 +147,9 @@ def link_collection_integration(headers, collection_uuid, integration_uuid):
 
 def setup_single_collection_integration(name, arn):
     """Creates an ASM collection and an integration, then links them."""
-    
+
     workflow = args.workflow
-    logging.debug(f"setup_single_collection_integration(name={name}, arn={arn}")
+    logging.debug(f"setup_single_collection_integration(name={name}, arn={arn})")
     collection = create_user_collection(headers, name, workflow)
     
     if args.dry_run:
@@ -147,22 +172,37 @@ def read_csv(file_path):
         reader = csv.reader(file)
 
         for row in reader:
-            # expects the ARN in the 2nd CSV column, and Name in the 4th column
-            # indexed from zero
-            setup_single_collection_integration(name=row[3], arn=row[1])
+            # expects the collection name in column 0, ARN in column 1
+            try:
+                setup_single_collection_integration(name=row[0], arn=row[1])
 
+                # Might help reduce random API errors when bulk loading at full speed
+                time.sleep(2)
+
+            except IndexError as e:
+                logging.error(f"IndexError while extracting fields from {file_path}, {e}") 
+                sys.exit(1)
 
 def main():
     """Parses command-line arguments and high level flow control."""
 
-    parser = argparse.ArgumentParser(description="Load AWS integrations in ASM.")
-    parser.add_argument( "-p", "--project-id", required=True, type=int, help="ASM Project ID")
-    parser.add_argument("-n", "--name", help="Name of the collection")
-    parser.add_argument("-a", "--arn", help="Name of the Mandiant ASM role in AWS")
-    parser.add_argument("-f", "--file", help="Path to CSV containing ARN,Name for bulk creation")
+    description="""Load AWS integrations in ASM, one integration per collection.
+
+Steps:
+1. Create new collection
+2. Create new integration
+3. Link integration to collection
+
+"""
+
+    parser = argparse.ArgumentParser(description=description)
+    parser.add_argument( "-p", "--project-id", required=True, type=int, help="Existing ASM integer project ID. ex: 24020")
+    parser.add_argument("-n", "--name", help="Name of the collection to create")
+    parser.add_argument("-a", "--arn", help="Full ARN of the existing Mandiant ASM role in AWS. ex: arn:aws:iam::7501232456:role/Mandiant-ASM-Access")
+    parser.add_argument("-f", "--file", help="Path to CSV containing ARN,Name for bulk creation. Columns: collection_name, role_arn")
     parser.add_argument( "-w", "--workflow", 
         default="authenticated_cloud_discovery",
-        help="Workflow name (e.g., external_discovery_and_assessment)",
+        help="Workflow type. Default: \"authenticated_cloud_discovery\". Reference: https://docs.mandiant.com/home/asm-create-a-collection#scan-workflow-templates",
     )
     parser.add_argument("-d", "--dry-run", action="store_true", default=False, 
         help="Dry run. Only print API calls."
@@ -181,6 +221,9 @@ def main():
     else:
         logging.getLogger().setLevel(logging.INFO)
         console_handler.setLevel(logging.INFO)
+
+    full_command = " ".join(sys.argv)
+    logging.info(f"Running as: {full_command}")
 
     if args.dry_run:
         logging.info("Starting dry run.")
