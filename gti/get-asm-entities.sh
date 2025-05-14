c0232604103904f980b4b2698ee489fb3e823dc8
Jacob <jacobm3@gmail.com>
2025-05-14 16:16:03 +0000

    add get-asm-entities.sh

---
+++ gti/get-asm-entities.sh
@@ -0,0 +1,7 @@
+curl --request GET \
+     --url https://www.virustotal.com/api/v3/asm/search/entities/type%3Agithubrepository \
+     --header 'accept: application/json' \
+     --header "x-apikey: $GTIKEY" 
+
+
+
