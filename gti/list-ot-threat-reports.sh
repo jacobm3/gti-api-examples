71496189a3b61a118dc59409e698039fe3b6cb61
Jacob <jacobm3@gmail.com>
2025-09-12 13:05:27 +0000

    add list-ot-threat-reports.sh

---
+++ gti/list-ot-threat-reports.sh
@@ -0,0 +1,10 @@
+API_KEY=$GTIKEY
+
+curl -s -X GET \
+"https://www.virustotal.com/api/v3/collections?filter=collection_type%3Areport%20name%3A%22OT%20Threat%22%20origin%3A%22Google%20Threat%20Intelligence%22&order=creation_date-" \
+-H "accept: application/json" \
+-H "x-apikey: $API_KEY"  \
+| jq '.data[] | {id: .id, name: .attributes.name}'
+
+#| jq '.data[] | {id: .id, name: .attributes.name, executive_summary: .attributes.executive_summary}'
+
